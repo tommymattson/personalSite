196828514a835214f021abd8534666c664af8ab3
Thomas Mattson <tdmattson21@gmail.com>
2026-02-09 11:28:19 -0600

    Commit 1

---
+++ main.js
@@ -0,0 +1,36 @@
+/* main.js */
+
+class MyHeader extends HTMLElement {
+    connectedCallback() {
+        // Check if we are inside the 'posts' folder
+        const isInPosts = window.location.pathname.includes('/posts/');
+        const prefix = isInPosts ? '../' : '';
+
+        this.innerHTML = `
+        <nav>
+            <a href="${prefix}index.html" class="logo">Thomas Mattson</a>
+            <div class="nav-links">
+                <a href="${prefix}index.html" id="link-home">Work</a>\
+                <a href="${prefix}blog.html" id="link-blog" style="color: var(--accent);">Blog</a>
+                <a href="${prefix}resume.html" id="link-resume">Resume</a>
+            </div>
+        </nav>
+        `;
+    }
+}
+
+class MyFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer>
+            <p>
+                <a href="https://linkedin.com/in/thomas-d-mattson" target="_blank" style="color: inherit;">LinkedIn</a> 
+                &bull; &copy; 2026 Thomas Mattson
+            </p>
+        </footer>
+        `;
+    }
+}
+
+customElements.define('my-header', MyHeader);
+customElements.define('my-footer', MyFooter);
